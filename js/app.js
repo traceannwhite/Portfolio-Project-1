d1473127a38a7dc4189731a77aecd2d154b66988
traceannwhite <traceannwhite@gmail.com>
2021-07-13 20:35:33 -0500

    Edited JS and Responsive Design

--- js/app.js
+++ js/app.js
@@ -21,19 +21,32 @@ $menuButton.on("click", (event) => {
 ////////////////////////
 
 //Project Cards JSON
-const $projects = $(".projects");
+const $projects = $(".projects-grid");
 $.ajax("./json/projects.json").then((data) => {
   data.forEach((project, index) => {
-    const $div = $("<div>");
+    const $div = $('<div class="project-card">');
     $div.html(
       `<h5 class ="project-title">${project.title}</h5>
       <img src='${project.image}'/>
       <p class="project-description">${project.description}</p>
-      <a href="#" class="live-link">${project.livelink}</a>`
+      <a href='${project.livelink} class="live-link">${project.livelink}</a>`
     );
     $projects.append($div);
+    // const $goToWebsite = $(".live-link");
+    //   $goToWebsite.on("click", () => {
+    //     window.location = $(".live-link")
+    //     return false 
+    //   })
+
   });
 });
+
+// Links project cards to website.
+// const $projectCard = $(".project-card");
+  $(".project-card").on("click", () => {
+    $(".live-link") = $(this).find(".live-link").atrr("href");
+    return false;
+  });
 
 ////////////////////////
 // FOOTER
